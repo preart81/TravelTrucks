a14e48022499fe6a3cf7616f37d6db4dcef48109
Artur Prepelytsia <preart81@gmail.com>
2024-10-27 23:40:28 +0200

    add Booking form

--- src/components/BookingForm/BookingForm.jsx
+++ src/components/BookingForm/BookingForm.jsx
@@ -1,6 +1,10 @@
 import { ErrorMessage, Field, Form, Formik } from 'formik';
+import { useState } from 'react';
+import DatePicker from 'react-datepicker';
+// import 'react-datepicker/dist/react-datepicker.css';
 import * as Yup from 'yup';
 import css from './BookingForm.module.css';
+import './react-datepicker.css';
 
 const BookingForm = () => {
   const validationSchema = Yup.object({
@@ -12,6 +16,8 @@ const BookingForm = () => {
     comment: Yup.string(),
   });
 
+  const [selectedDate, setSelectedDate] = useState(null);
+
   return (
     <div className={css.containerBooking}>
       <h3 className={css.title}>Book your campervan now</h3>
@@ -21,59 +27,80 @@ const BookingForm = () => {
       <Formik
         initialValues={{ name: '', email: '', date: '', comment: '' }}
         validationSchema={validationSchema}
-        onSubmit={values => {
+        onSubmit={(values, { resetForm }) => {
           console.log('Form values:', values);
+          resetForm();
+          setSelectedDate(null);
         }}
       >
-        <Form className={css.form}>
-          <div className={css.fieldContainer}>
-            <Field
-              name="name"
-              type="text"
-              placeholder="Name*"
-              className={css.input}
-            />
-            <ErrorMessage name="name" component="div" className={css.error} />
-          </div>
+        {({ setFieldValue }) => (
+          <Form className={css.form}>
+            <div className={css.fieldContainer}>
+              <Field
+                name="name"
+                type="text"
+                placeholder="Name*"
+                className={css.input}
+              />
+              <ErrorMessage name="name" component="div" className={css.error} />
+            </div>
 
-          <div className={css.fieldContainer}>
-            <Field
-              name="email"
-              type="email"
-              placeholder="Email*"
-              className={css.input}
-            />
-            <ErrorMessage name="email" component="div" className={css.error} />
-          </div>
+            <div className={css.fieldContainer}>
+              <Field
+                name="email"
+                type="email"
+                placeholder="Email*"
+                className={css.input}
+              />
+              <ErrorMessage
+                name="email"
+                component="div"
+                className={css.error}
+              />
+            </div>
 
-          <div className={css.fieldContainer}>
-            <Field
-              name="date"
-              type="date"
-              placeholder="Booking Date*"
-              className={css.input}
-            />
-            <ErrorMessage name="date" component="div" className={css.error} />
-          </div>
+            <div className={css.fieldContainer}>
+              <DatePicker
+                formatWeekDay={nameOfDay =>
+                  nameOfDay.substr(0, 3).toUpperCase()
+                }
+                dateFormat={'dd/MM/yyyy'}
+                selected={selectedDate}
+                onChange={date => {
+                  setSelectedDate(date);
+                  setFieldValue('date', date);
+                }}
+                placeholderText="Booking Date*"
+                className={css.input}
+                style={{}}
+              />
 
-          <div className={css.fieldContainer}>
-            <Field
-              name="comment"
-              as="textarea"
-              placeholder="Comment"
-              className={css.input}
-            />
-            <ErrorMessage
-              name="comment"
-              component="div"
-              className={css.error}
-            />
-          </div>
+              <ErrorMessage name="date" component="div" className={css.error} />
+            </div>
 
-          <button type="submit" className="submitButton" style={{ marginTop: '4px' }}>
-            Send
-          </button>
-        </Form>
+            <div className={css.fieldContainer}>
+              <Field
+                name="comment"
+                as="textarea"
+                placeholder="Comment"
+                className={css.input}
+              />
+              <ErrorMessage
+                name="comment"
+                component="div"
+                className={css.error}
+              />
+            </div>
+
+            <button
+              type="submit"
+              className="submitButton"
+              style={{ marginTop: '4px' }}
+            >
+              Send
+            </button>
+          </Form>
+        )}
       </Formik>
     </div>
   );
